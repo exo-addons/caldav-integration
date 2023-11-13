9b5c4004ad982c8480b5097391f5fbcf5edd1f0e
Hedi Rdid <rdidmohamedelhadi@gmail.com>
2023-11-13 11:45:43 +0100

    feat: add Caldav Settings Drawer & config portlet caldav-integration

--- caldav-integration-webapp/src/main/webapp/vue-app/caldav-integration/initComponents.js
+++ caldav-integration-webapp/src/main/webapp/vue-app/caldav-integration/initComponents.js
@@ -1,4 +1,27 @@
+/*
+ * Copyright (C) 2023 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import AgendaConnectors from './components/AgendaConnectors.vue';
+import AgendaConnectorsAlert from './components/AgendaConnectorsAlert.vue';
+import CaldavSettingsDrawer from './components/CaldavSettingsDrawer.vue';
+
 const components = {
+  'caldav-settings-drawer': CaldavSettingsDrawer,
+  'agenda-connectors': AgendaConnectors,
+  'agenda-connectors-alert': AgendaConnectorsAlert
 };
 
 for (const key in components) {
