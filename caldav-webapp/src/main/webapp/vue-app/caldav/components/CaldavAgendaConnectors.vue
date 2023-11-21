afb17c1e0ec8c41cf4d78639367b15fa775e8343
Romain Dénarié <romain.denarie@exoplatform.com>
2023-11-21 23:04:18 +0100

    Use js group module for caldav extension

--- caldav-webapp/src/main/webapp/vue-app/caldav/components/CaldavAgendaConnectors.vue
+++ caldav-webapp/src/main/webapp/vue-app/caldav/components/CaldavAgendaConnectors.vue
@@ -24,8 +24,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 <script>
 
 export default {
-  created() {
-    document.addEventListener('open-caldav-connector-settings-drawer',this.openCaldavDrawer);
+  mounted() {
+    this.openCaldavDrawer();
   },
   methods: {
     openCaldavDrawer() {
